95629af53ccccf96c97d774e7cee695056e27897
hj37 <54181059+hj37@users.noreply.github.com>
2019-09-12 17:53:01 +0900

    구분석 작업 & 노트 오브젝트 생성 및 이동

---
+++ 2DRythmGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{//싱글톤 기법으로 구현함
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);//오류발견시 파괴
+    }
+
+    public float noteSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2DRythmGame/Assets/Scripts/NoteBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int noteType;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed); //노트가 정해진 스피드로 매프레임마다 내려감 
+    }
+}
