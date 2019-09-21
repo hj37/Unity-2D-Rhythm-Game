a8aee6507648e067efdd6c61a4c7636c49e4f5c0
hj37 <54181059+hj37@users.noreply.github.com>
2019-09-21 20:13:12 +0900

    노드 이펙트

---
+++ 실습1/Assets/MainCamera.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using System.IO.Ports;
+
+
+
+public class MainCamera : MonoBehaviour
+{
+
+
+
+    SerialPort sp = new SerialPort("COM3", 115200);
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+
+        sp.Open();
+
+        sp.ReadTimeout = 1;
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        if (sp.IsOpen)
+
+        {
+
+            try
+
+            {
+
+                string value = sp.ReadLine();
+
+                print(value);
+
+                sp.BaseStream.Flush();
+
+            }
+
+            catch (System.Exception)
+
+            {
+
+
+
+            }
+
+        }
+
+
+
+    }
+
+}
